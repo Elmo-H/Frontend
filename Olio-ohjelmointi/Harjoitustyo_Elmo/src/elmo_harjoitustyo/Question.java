25fc0492ef4bed7ffc216570c836f92eb52aab69
Elmo-H <elmo.huoku@gmail.com>
2022-01-04 21:59:25 +0200

    Updated versions of exercises

--- Olio-ohjelmointi/Harjoitustyo_Elmo/src/elmo_harjoitustyo/Question.java
+++ Olio-ohjelmointi/Harjoitustyo_Elmo/src/elmo_harjoitustyo/Question.java
@@ -2,37 +2,25 @@ package elmo_harjoitustyo;
 import java.util.*;
 //Rakentaja metodi Question
 public class Question {
+	
+	//Esitell‰‰n j‰senmuuttujat
+	Scanner input = new Scanner(System.in);
+    Random multiple = new Random();
+    int num1 = 1 + multiple.nextInt(9);
+    int num2 = 1 + multiple.nextInt(9);
 
     public void Ask() {
     	
-    	//Luodaan metodi jolla saa kysymykset satunnaiseksi
-    	Random multiple = new Random();
-    	
-    	//Esitell‰‰n j‰senmuuttujat
-    	Scanner input = new Scanner(System.in);
-        boolean wrong = true;
-        int num1 = 1 + multiple.nextInt(9);
-        int num2 = 1 + multiple.nextInt(9);
+    	//Kysyt‰‰n k‰ytt‰j‰lt‰ kertotauluja
+        System.out.println(askQuestion(num1, num2));
+        int answer = input.nextInt();
         
-        //Tehd‰‰n looppi
-        while(wrong == true) {
-        	
-        	//Kysyt‰‰n k‰ytt‰j‰lt‰ kertotauluja
-	        System.out.println(askQuestion(num1, num2));
-	        int answer = input.nextInt();
-	        
-	        //Luodaan if else looppi jossa testataan onko vastaus oikein
-	        //ja jos on se palautuu ohjelman alkuun ja kysyy uudestaan
-	        if(answer == num1 * num2) {
-	            System.out.println("Oikea vastaus!");
-	            wrong = false;
-	            Ask();
-	            }
-	
-	        else {
-	            System.out.println("V‰‰r‰ vastaus. Yrit‰ uudelleen");
-	        }
+        //Luodaan looppi jossa testataan onko vastaus oikein
+        //ja jos on se palautuu ohjelman alkuun ja kysyy uudestaan
+        while(answer != num1 * num2) {    
+        	System.out.println("Vastasit v‰‰rin!");
         }
+        System.out.println("Oikea vastaus!");
         //Suljetaan skanneri, jotta se ei herjaa turhaan
         input.close();
     }
